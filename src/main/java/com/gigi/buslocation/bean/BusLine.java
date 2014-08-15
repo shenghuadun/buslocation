a68039b9d4d271d9a565ba19b667b5a0377c1138
shenghuadun <shengguolin123@163.com>
2014-08-15 22:07:12 +0800

    添加busline类

---
+++ src/main/java/com/gigi/buslocation/bean/BusLine.java
@@ -0,0 +1,219 @@
+package com.gigi.buslocation.bean;
+
+
+public class BusLine
+{
+
+	/**
+	 * 线路id
+	 */
+	private String lineId;
+	
+	/**
+	 * 线路名称
+	 */
+	private String lineName;
+	
+	/**
+	 * 搜索关键字
+	 */
+	private String searchKey;
+	
+	/**
+	 * 主要站点描述
+	 */
+	private String mainStationsDesc;
+	
+	/**
+	 * 下行描述
+	 */
+	private String downDesc;
+	
+	/**
+	 * 上行描述
+	 */
+	private String upDesc;
+
+	/**
+	 * 下行工作时间
+	 */
+	private String downAvilableTime;
+	
+	/**
+	 * 上行工作时间
+	 */
+	private String upAvilableTime;
+
+	/**
+	 * 线路全长
+	 */
+	private String totalLength;
+	
+	/**
+	 * 票价信息
+	 */
+	private String price;
+
+	/*********************************************************************
+	 * 
+	 * 数据库列名常量列表
+	 * 
+	 *********************************************************************/
+
+	public String getLineId()
+	{
+		return lineId;
+	}
+
+	public void setLineId(String lineId)
+	{
+		this.lineId = lineId;
+	}
+
+	public String getLineName()
+	{
+		return lineName;
+	}
+
+	public void setLineName(String lineName)
+	{
+		this.lineName = lineName;
+	}
+
+	public String getSearchKey()
+	{
+		return searchKey;
+	}
+
+	public void setSearchKey(String searchKey)
+	{
+		this.searchKey = searchKey;
+	}
+
+	public String getMainStationsDesc()
+	{
+		return mainStationsDesc;
+	}
+
+	public void setMainStationsDesc(String mainStationsDesc)
+	{
+		this.mainStationsDesc = mainStationsDesc;
+	}
+
+	public String getDownDesc()
+	{
+		return downDesc;
+	}
+
+	public void setDownDesc(String downDesc)
+	{
+		this.downDesc = downDesc;
+	}
+
+	public String getUpDesc()
+	{
+		return upDesc;
+	}
+
+	public void setUpDesc(String upDesc)
+	{
+		this.upDesc = upDesc;
+	}
+
+	public String getDownAvilableTime()
+	{
+		return downAvilableTime;
+	}
+
+	public void setDownAvilableTime(String downAvilableTime)
+	{
+		this.downAvilableTime = downAvilableTime;
+	}
+
+	public String getUpAvilableTime()
+	{
+		return upAvilableTime;
+	}
+
+	public void setUpAvilableTime(String upAvilableTime)
+	{
+		this.upAvilableTime = upAvilableTime;
+	}
+
+	public String getTotalLength()
+	{
+		return totalLength;
+	}
+
+	public void setTotalLength(String totalLength)
+	{
+		this.totalLength = totalLength;
+	}
+
+	public String getPrice()
+	{
+		return price;
+	}
+
+	public void setPrice(String price)
+	{
+		this.price = price;
+	}
+
+	/**
+	 * 线路id
+	 */
+	public static final String LINEID = "lineId";
+	
+	/**
+	 * 线路名称
+	 */
+	public static final String LINENAME = "lineName";
+	
+	/**
+	 * 搜索关键字
+	 */
+	public static final String SEARCHKEY = "searchKey";
+	
+	/**
+	 * 主要站点描述
+	 */
+	public static final String MAINSTATIONSDESC = "mainStationsDesc";
+	
+	/**
+	 * 下行描述
+	 */
+	public static final String DOWNDESC = "downDesc";
+	
+	/**
+	 * 上行描述
+	 */
+	public static final String UPDESC = "upDesc";
+
+	/**
+	 * 下行工作时间
+	 */
+	public static final String DOWNAVILABLETIME = "downAvilableTime";
+	
+	/**
+	 * 上行工作时间
+	 */
+	public static final String UPAVILABLETIME = "upAvilableTime";
+
+	/**
+	 * 线路全长
+	 */
+	public static final String TOTALLENGTH = "totalLength";
+	
+	/**
+	 * 票价信息
+	 */
+	public static final String PRICE = "price";
+
+	@Override
+	public String toString()
+	{
+		return "【线路名称：】" + lineName + " 【线路ID:】" + lineId + " 【线路描述：】" + mainStationsDesc;
+	}
+
+}
